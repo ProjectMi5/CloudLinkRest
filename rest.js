49fb8e3e1df69343bd801ab3379072625a9fd9a1
Thomas Frei <mail@thomasfrei.de>
2015-11-20 01:55:55 +0100

    add placeOrderGet (rest and test)

--- rest.js
+++ rest.js
@@ -1,11 +1,8 @@
-/**
- *
- * @type {*|exports|module.exports}
- */
 var urljoin = require('url-join');
 var request = require('request');
 var Promise = require('bluebird');
-var logger  = require('./logger');
+
+var logger = require('./logger');
 
 function MI5REST(host, user, password){
   this.host = host; // http(s)://x.y.com/
@@ -13,16 +10,16 @@ function MI5REST(host, user, password){
   this.password = password; // basic auth
 
   this.rejectUnauthorized = false; // Default param
-  //this.verbose = true; // log or not
+  this.verbose = true; // log or not
 }
 module.exports = MI5REST;
 
 MI5REST.prototype.checkConnection = function(){
-  var options = this._modifyRequestOptions({
-    url: this._url('helloWorld')
+  var options = this._options({
+    target: 'helloWorld'
   });
 
-  logger.info('/checkConnection');
+  console.log('/checkConnection');
   logger.debug(options);
 
   return this._GetRequest(options)
@@ -32,8 +29,8 @@ MI5REST.prototype.checkConnection = function(){
 };
 
 MI5REST.prototype.getOrdersByStatus = function(status){
-  var options = this._modifyRequestOptions({
-    url:  this._url('getOrdersByStatus'),
+  var options = this._options({
+    target: 'getOrdersByStatus',
     form: {status: status}
   });
 
@@ -45,8 +42,8 @@ MI5REST.prototype.getOrdersByStatus = function(status){
 };
 
 MI5REST.prototype.placeOrder = function(order){
-  var options = this._modifyRequestOptions({
-    url:  this._url('placeOrder'),
+  var options = this._options({
+    target: 'placeOrder',
     form: {order: JSON.stringify(order)}
   });
 
@@ -57,6 +54,45 @@ MI5REST.prototype.placeOrder = function(order){
     .then(this._safeJsonParse);
 };
 
+MI5REST.prototype.placeOrderGet = function(order){
+  var options = this._options({
+    target: 'placeOrder/'+order.recipeId+'/'+JSON.stringify(order.parameters)+'/'+order.marketPlaceId,
+  });
+
+  logger.info('/placeOrderGet', order);
+  logger.debug(options);
+
+  return this._GetRequest(options)
+    .then(this._safeJsonParse);
+};
+
+MI5REST.prototype.updateOrderStatus = function(orderid, status){
+  var options = this._options({
+    target: 'updateOrderStatus',
+    form: {id: orderid, status: status}
+  });
+
+  logger.info('/updateOrderStatus', orderid, status);
+  logger.debug(options);
+
+  return this._PostRequest(options)
+    .then(this._safeJsonParse);
+};
+
+MI5REST.prototype.updateOrder = function(order){
+  var options = this._options({
+    target: 'updateOrder',
+    form: {order: JSON.stringify(order)}
+  });
+
+  logger.info('/updateOrder', order);
+  logger.debug(options);
+
+  return this._PostRequest(options)
+    .then(this._safeJsonParse);
+};
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////// Helper ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -65,7 +101,8 @@ MI5REST.prototype._url = function(target){
   return urljoin(this.host, target);
 };
 
-MI5REST.prototype._modifyRequestOptions = function(options){
+MI5REST.prototype._options = function(options){
+  options.url = this._url(options.target);
   options.auth = {
     user :      this.user,
     password :  this.password
@@ -104,55 +141,4 @@ MI5REST.prototype._safeJsonParse = function(body){
     //return err
     return new Promise(function(res, rej){ rej(err);});
   }
-};
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-MI5REST.prototype.updateOrderStatus = function(orderid, status){
-  var options = {
-    url:  urljoin(config.MI5REST.host, config.MI5REST.updateOrderStatus),
-    rejectUnauthorized: false, // TODO certificate needs to be bundled correctly
-    form: {id: orderid, status: status},
-    auth: config.auth
-  };
-
-  return new Promise(function(resolve, reject){
-    request.post(options, function(err, res, body){
-      if(err) reject(err);
-      try {
-        body = JSON.parse(body);
-        resolve(body);
-      } catch (err){
-        console.log(err, body);
-        reject('problems in JSON.parse, probably return is not JSON formatted');
-      }
-    });
-  });
-};
-
-MI5REST.prototype.updateOrder = function(order){
-  var options = {
-    url:  urljoin(config.MI5REST.host, config.MI5REST.updateOrder),
-    rejectUnauthorized: false, // TODO certificate needs to be bundled correctly
-    form: {order: JSON.stringify(order)},
-    auth: config.auth
-  };
-
-  return new Promise(function(resolve, reject){
-    request.post(options, function(err, res, body){
-      if(err) reject(err);
-      try {
-        body = JSON.stringify(body);
-        resolve(body);
-      } catch (err){
-        console.log(err, body);
-        reject('problems in JSON.stringify, probably return is not JSON formatted');
-      }
-    });
-  });
 };
--- test/test-rest.js
+++ test/test-rest.js
@@ -21,6 +21,49 @@ test('placeOrder', function(t){
   return mi5Rest.placeOrder({recipeId: 10010, parameters: [], marketPlaceId: 'eu'})
     .then(function(body){
       t.equal(body.status, 'ok', 'status should be ok');
+      t.assert(!isNaN(body.orderId), 'orderId must be a number');
       t.equal(body.orderStatus, 'pending', 'new orders are always pending');
     });
 });
+
+test('updateOrderStatus', function(t){
+  return mi5Rest.placeOrder({recipeId: 10010, parameters: [], marketPlaceId: 'eu'})
+    .then(function(body){
+      return mi5Rest.updateOrderStatus(body.orderId, 'accepted');
+    })
+    .then(function(body){
+      t.equal(body.status, 'ok', 'status should be ok');
+    });
+});
+
+test('updateOrderStatus', function(t){
+  return mi5Rest.placeOrder({recipeId: 10010, parameters: [], marketPlaceId: 'eu'})
+    .then(function(body){
+      return mi5Rest.updateOrderStatus(body.orderId, 'accepted');
+    })
+    .then(function(body){
+      t.equal(body.status, 'ok', 'status should be ok');
+    });
+});
+
+test('updateOrder', function(t){
+  return mi5Rest.placeOrder({recipeId: 10010, parameters: [], marketPlaceId: 'eu'})
+    .then(function(body){
+      var order = {
+        orderId: body.orderId,
+        status: 'done'
+      };
+      return mi5Rest.updateOrder(order);
+    })
+    .then(function(body){
+      t.equal(body.status, 'ok', 'status should be ok');
+    });
+});
+
+test.only('placeOrderGet', function(t){
+  return mi5Rest.placeOrderGet({recipeId: 10051, parameters:[100,40,30,10,1], marketPlaceId: 'eu'})
+    .then(function(body){
+      t.equal(body.status, 'ok', 'status of request: ok');
+      t.equal(body.orderStatus, 'pending', 'orderStatus pending');
+    })
+})
