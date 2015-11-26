0fa5c95fd8355d6da5ae27abd76adc92eb129fe6
Thomas Frei <mail@thomasfrei.de>
2015-11-26 10:34:03 +0100

    do not generate log-file

--- logger.js
+++ logger.js
@@ -20,15 +20,15 @@ winston.emitErrs = true;
 
 var logger = new winston.Logger({
   transports: [
-    new winston.transports.File({
-      level: 'info',
-      filename: __dirname+'/log/logger.log',
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, //5MB
-      maxFiles: 5,
-      colorize: false
-    }),
+    //new winston.transports.File({
+    //  level: 'info',
+    //  filename: __dirname+'/log/logger.log',
+    //  handleExceptions: true,
+    //  json: true,
+    //  maxsize: 5242880, //5MB
+    //  maxFiles: 5,
+    //  colorize: false
+    //}),
     new winston.transports.Console({
       level: 'debug',
       handleExceptions: true,
