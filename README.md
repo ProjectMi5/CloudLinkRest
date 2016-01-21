bb880171463886d9724c6d672e5b50fc962a662b
Thomas Frei <mail@thomasfrei.de>
2016-01-21 10:30:44 +0100

    update Readme and set test to local test

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 CloudLinkRest library
 =============
 
-Basic API
------------
+## Installation
 
-* checkConnection
-* placeOrder
-* getOrdersByStatus
+`npm install`
 
+## Run the Tests:
+
+* Make sure that CloudLink is running (or reachable over the network)
+* Set `process.env.TEST = true;` (this will do the unit-tests locally)
+* Run `npm test`
+
+## Basic API (see code)
+
+See the code `rest.js` for a complete referene.
--- runTests.js
+++ runTests.js
@@ -5,15 +5,15 @@
 
 // Set Test env variable: Windows > set TEST=true
 // Reset it in Windows: > set TEST=
-//process.env.TEST = true; //run it with test configurations if not setting env variables
+process.env.TEST = true; //run it with test configurations if not setting env variables
 
-//require('./test/test-orderAllRecipes');
-//require('./test/test-feedback');
-//require('./test/test-devices');
-//require('./test/test-recipe');
-//require('./test/test-misc');
-//require('./test/test-worker');
+require('./test/test-orderAllRecipes');
+require('./test/test-feedback');
+require('./test/test-devices');
+require('./test/test-recipe');
+require('./test/test-misc');
+require('./test/test-worker');
 require('./test/test-worker-loadOrders');
-//require('./test/test-qr');
-//require('./test/test-jobboard');
-//require('./test/test-placeOrderGet');
+require('./test/test-qr');
+require('./test/test-jobboard');
+require('./test/test-placeOrderGet');
